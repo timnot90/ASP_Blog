5afb089a188d2b5b9b28d708bd9a157ea71dff06
timnot90 <timo.notheisen@googlemail.com>
2014-05-02 20:03:24 +0200

    added missing files

---
+++ Blog.Web/Areas/Administration/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Web.Areas.Administration.Models;
+using Blog.Web.Services;
+
+namespace Blog.Web.Areas.Administration.Controllers
+{
+    public class AdminController : Controller
+    {
+        IBlogService _service = new BlogService();
+
+        public ActionResult Index()
+        {
+            AdminModel model = new AdminModel();
+            model.Users = _service.GetAllUserProfiles();
+            return View(model);
+        }
+
+        public ActionResult ChangeRole()
+        {
+            return null;
+        }
+    }
+}
---
+++ Blog.Web/Models/Account/RegisterModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Blog.Core.DataAccess.Blog;
+
+namespace Blog.Web.Models.Account
+{
+    public class RegisterModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Username")]
+        [Required(ErrorMessage = "The username must be declared.")]
+        public string UserName { get; set; }
+
+        [DisplayName("NewPassword")]
+        [Required(ErrorMessage = "A password must be declared.")]
+        public string Password { get; set; }
+
+        [DisplayName("Confirm Password")]
+        [Required(ErrorMessage = "Your password must be confirmed.")]
+        public string PasswordConfirmed { get; set; }
+
+        [DisplayName("E-Mail")]
+        [EmailAddress(ErrorMessage = "E-Mail falsch")]
+//        [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$", ErrorMessage = "The E-Mail Adress is not valid.")]
+        [Required(ErrorMessage = "An E-Mail adress must be declared.")]
+        public string Email { get; set; }
+
+        public RegisterModel()
+        {
+        }
+
+        public RegisterModel(UserProfile userProfile)
+        {
+            UpdateModel(userProfile);
+        }
+
+        public void UpdateSource(UserProfile source)
+        {
+            source.UserName = UserName;
+            source.Email = Email;
+        }
+
+        public void UpdateModel(UserProfile source)
+        {
+            Id = source.ID;
+            UserName = source.UserName;
+            Email = source.Email;
+        }
+
+    }
+}
