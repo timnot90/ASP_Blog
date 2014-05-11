173e1cd05facceaf4905d85a0674c56101e6dc8e
timnot90 <timo.notheisen@googlemail.com>
2014-05-11 16:35:19 +0200

    ...

---
+++ Blog.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Blog.Web.Areas.Administration.Models;
+using Blog.Web.Services;
+
+namespace Blog.Web.Areas.Administration.Controllers
+{
+    [Authorize(Roles = CustomRoles.Administrator)]
+    public class AdministrationController : Controller
+    {
+        IBlogService _service = new BlogService();
+
+        public ActionResult Index()
+        {
+            AdministrationModel model = new AdministrationModel();
+            model.Users = _service.GetAllUserProfiles();
+            return View(model);
+        }
+
+        public ActionResult ChangeRole(string username, string newRole)
+        {
+            if (!Roles.GetRolesForUser(username).Contains(newRole))
+            {
+                Roles.AddUserToRole(username, newRole);
+            }
+            else
+            {
+                Roles.RemoveUserFromRole(username, newRole);
+            }
+            return null;
+        }
+
+        public ActionResult _UserListItem()
+        {
+            return View();
+        }
+    }
+}
