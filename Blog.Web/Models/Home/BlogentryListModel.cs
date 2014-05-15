ad1e1409958a272dc906313cf688da5ecf4ba562
timnot90 <timo.notheisen@googlemail.com>
2014-05-15 08:07:43 +0200

    ...

---
+++ Blog.Web/Models/Home/BlogentryListModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Web.Models.Home
+{
+    public class BlogentryListModel
+    {
+        public List<BlogEntryListItemModel> Blogentries { get; set; }
+        public FilterModel Filter { get; set; }
+
+        public BlogentryListModel()
+        {
+        }
+
+        public BlogentryListModel( List<BlogEntryListItemModel>  entries)
+        {
+            this.Blogentries = entries;
+        }
+    }
+}
